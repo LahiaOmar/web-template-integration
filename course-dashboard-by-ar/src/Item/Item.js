8cd42fff6e0dfd656490a5f8edc1fcb0394d6d39
LahiaOmar <omar.lahia@gmail.com>
2020-11-14 17:45:28 +0100

    imp progress bar ui

--- course-dashboard-by-ar/src/Item/Item.js
+++ course-dashboard-by-ar/src/Item/Item.js
@@ -1,26 +1,45 @@
 import React from 'react'
 
-const Item = ({ topTitle, bottomTitle, icon, progBar }) => {
-  const progress = progBar ? progBar.progress : 0;
+function ProgressBar({ progBar }) {
   let barClassName = "progress-bar"
-  if (progress === 0) {
+  const progress = progBar ? progBar.progress : 0
+  const barLength = 80
+  if (progBar === undefined) {
     barClassName += " bar-no-active"
   }
-  console.log(progress, barClassName)
+  let firstDefaultBgColor = progBar && progBar.firstColor ? progBar.firstColor : "gba(94, 129, 244)"
+  let seconfDefaultBgColor = progBar && progBar.secondColor ? progBar.secondColor : "gba(94, 129, 244, 0.2)"
+
+  return (
+    <div className={barClassName}
+      style={{
+        backgroundColor: seconfDefaultBgColor,
+        width: `${barLength}px`
+      }}
+    >
+      {progBar &&
+        <span style={{
+          backgroundColor: firstDefaultBgColor,
+          width: `${progress}px`
+        }}></span>
+      }
+    </div>
+  )
+}
+
+const Item = ({ topTitle, bottomTitle, icon, progBar, logoStyle }) => {
+  const progress = progBar ? progBar.progress : 0;
+
   return (
     <div className="item">
-      <div className="item-logo"> <img src={icon} /> </div>
+      <div className="item-logo" style={{ ...logoStyle }}>
+        <img src={icon} />
+      </div>
       <div className="item-title">
         <p className="item-title-top">{topTitle}</p>
         <p className="item-title-bottom">{bottomTitle}</p>
       </div>
-      <div className={barClassName}>
-        {progBar &&
-          <span style={{
-            width: `${progress}px`
-          }}></span>
-        }
-      </div>
+      <ProgressBar progBar={progBar} />
 
     </div>
   )
--- course-dashboard-by-ar/src/UserInformations/UserInformations.js
+++ course-dashboard-by-ar/src/UserInformations/UserInformations.js
@@ -19,22 +19,55 @@ const UserInformations = () => {
         <Item topTitle="UX/UI - Design" bottomTitle="Advanced" icon={uxUi}
           progBar={{
             progress: 50,
+            firstColor: "rgba(94, 129, 244, 1)",
+            secondColor: "rgba(94, 129, 244, 0.2)"
           }}
+          logoStyle={
+            {
+              backgroundColor: '#EEF2FD'
+            }
+          }
         />
         <Item topTitle="Photography" bottomTitle="Intermediate" icon={photographyIcon}
           progBar={{
             progress: 50,
+            firstColor: 'rgba(12,195,231,1)',
+            secondColor: 'rgba(12, 195, 231, 0.18)'
           }}
+          logoStyle={
+            {
+              backgroundColor: '#D5F5FB'
+            }
+          }
         />
         <Item topTitle="Animation" bottomTitle="Advance" icon={animationIcon}
           progBar={{
             progress: 50,
+            firstColor: 'rgba(255, 128, 139, 1)',
+            secondColor: 'rgba(255, 128, 139, 0.2)'
           }}
+          logoStyle={
+            {
+              backgroundColor: '#FFEFD6'
+            }
+          }
         />
       </ItemContainer>
       <ItemContainer title="Upcoming Task">
-        <Item topTitle="UX/UI - Discussion" bottomTitle="27 Oct 2020, Tuesday" icon={discussion} />
-        <Item topTitle="Animation - 3D Animation" bottomTitle="27 october 2020, Tuesday" icon={animation3D} />
+        <Item topTitle="UX/UI - Discussion" bottomTitle="27 Oct 2020, Tuesday"
+          icon={discussion}
+          logoStyle={
+            {
+              backgroundColor: '#EEF2FD'
+            }
+          } />
+        <Item topTitle="Animation - 3D Animation" bottomTitle="27 october 2020, Tuesday"
+          icon={animation3D}
+          logoStyle={
+            {
+              backgroundColor: '#FFEFD6'
+            }
+          } />
       </ItemContainer>
       <AppDownload />
     </div>
